d01e062600cdb856cb64989e55ac8e82e49ef779
TaranWanderer42 <60717670+TaranWanderer42@users.noreply.github.com>
2020-04-01 16:17:40 -0400

    First commit

---
+++ City Voice/Notifications.swift
@@ -0,0 +1,11 @@
+//
+//  Notifications.swift
+//  City Voice
+//
+//  Created by Declan Bradley on 3/29/20.
+//  Copyright © 2020 The City Voice. All rights reserved.
+//
+
+
+
+
---
+++ City Voice/UNUserNotificationCenter.swift
@@ -0,0 +1,10 @@
+//
+//  UNUserNotificationCenter.swift
+//  City Voice
+//
+//  Created by Declan Bradley on 3/30/20.
+//  Copyright © 2020 The City Voice. All rights reserved.
+//
+
+import Foundation
+{class UNUserNotificationCenter : NSObject}
